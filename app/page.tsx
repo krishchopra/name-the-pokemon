2469512ce7ce575ad00c06f99672c974fc1f5ef4
Krish Chopra <krish.chopra23@gmail.com>
2024-08-21 00:20:48 -0400

    apostrophe...

--- app/page.tsx
+++ app/page.tsx
@@ -11,7 +11,7 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center p-24">
       <div className="text-center">
-        <h1 className="text-4xl font-bold">Who's that Pokémon?</h1>
+        <h1 className="text-4xl font-bold">Who&rsquo;s that Pokémon?</h1>
         <p className="mt-4 text-2xl">{randomPokemon}</p>
         <div className="flex justify-center mt-4">
           <Image src={imageUrl} alt={randomPokemon} width={200} height={200} />
