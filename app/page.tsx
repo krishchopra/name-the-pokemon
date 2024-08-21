873842fb1afc8ad3d4caa00ac6fbe0e7b93585fc
Krish Chopra <krish.chopra23@gmail.com>
2024-08-21 00:34:10 -0400

    more edits

--- app/page.tsx
+++ app/page.tsx
@@ -1,35 +1,16 @@
-'use client';
-
-import { useEffect, useState } from "react";
 import Image from "next/image";
-import { pokemonData } from "./components/data";
+import { getRandomPokemon } from "./utils/getRandomPokemon";
 
-export default function Home() {
-  const [randomPokemon, setRandomPokemon] = useState<string | null>(null);
-  const [imageUrl, setImageUrl] = useState<string | null>(null);
-
-  useEffect(() => {
-    const pokemonNames = Object.keys(pokemonData);
-    const selectedPokemon = pokemonNames[Math.floor(Math.random() * pokemonNames.length)];
-    const randomPokemonNumber = pokemonData[selectedPokemon];
-    const randomPokemonNumberString = randomPokemonNumber.toString().padStart(3, '0');
-    const imageUrl = `https://www.pokemon.com/static-assets/content-assets/cms2/img/pokedex/detail/${randomPokemonNumberString}.png`;
-
-    setRandomPokemon(selectedPokemon);
-    setImageUrl(imageUrl);
-  }, []);
-
-  if (!randomPokemon || !imageUrl) {
-    return <div>Loading...</div>;
-  }
+export default async function Home() {
+  const { selectedPokemon, imageUrl } = await getRandomPokemon();
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-center p-24">
       <div className="text-center">
         <h1 className="text-4xl font-bold">Who&rsquo;s that Pokémon?</h1>
-        <p className="mt-4 text-2xl">{randomPokemon}</p>
+        <p className="mt-6 text-2xl">{selectedPokemon}</p>
         <div className="flex justify-center mt-4">
-          <Image src={imageUrl} alt={randomPokemon} width={200} height={200} />
+          <Image src={imageUrl} alt={selectedPokemon} width={200} height={200} />
         </div>
       </div>
     </main>
