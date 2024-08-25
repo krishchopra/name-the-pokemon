c66dbb9243e66940036c80518b1c4502b1f527d2
Krish Chopra <64521969+krishchopra@users.noreply.github.com>
2024-08-25 16:01:44 -0400

    Delete app directory

--- app/utils/getServerSidePokemon.ts
+++
@@ -1,6 +0,0 @@
-import { cache } from 'react'
-import { getRandomPokemon } from './getRandomPokemon'
-
-export const getServerSidePokemon = cache(async () => {
-  return await getRandomPokemon()
-})
